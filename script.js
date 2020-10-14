5907613c0a6cfa484582ac41156fcf11e98aeada
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-14 12:24:28 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-document.getElementById("hi").src = "https://media1.tenor.com/images/7fdf8192ea03730d839f48f942112021/tenor.gif?itemid=16070760"
+function calculate() {
+  document.getElementById("hi").src = "https://media1.tenor.com/images/7fdf8192ea03730d839f48f942112021/tenor.gif?itemid=16070760"
+}
