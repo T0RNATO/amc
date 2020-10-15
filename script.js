83499084cb6470fb2c92808d1e617aabf254021c
T0RNATO <62496453+T0RNATO@users.noreply.github.com>
2020-10-15 14:27:24 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var x = 0;
 function calculate() {
-  document.getElementById("hi").src = "https://media1.tenor.com/images/7fdf8192ea03730d839f48f942112021/tenor.gif?itemid=16070760"
+  if(x == 0) {
+    document.getElementById("hi").src = "https://media1.tenor.com/images/7fdf8192ea03730d839f48f942112021/tenor.gif?itemid=16070760";
+  }
+  if(x == 1) {
+    document.getElementById("hi").src = "";
+  }
+  x = 1 - x;
 }
